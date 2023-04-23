704f4a5dd2a59e577c13494338fef1fb7be37ec3
Valmith <kvrhettlaurea@gmail.com>
2023-04-23 18:49:23 +0800

    made a homescreen folder

--- lib/Homescreen/homescreen.dart
+++ lib/Homescreen/homescreen.dart
@@ -1,18 +1,7 @@
+
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'The Minecraft Almanac',
-      home: ImagePage(),
-    );
-  }
-}
 
 class ImagePage extends StatelessWidget {
   final List<String> images = [
